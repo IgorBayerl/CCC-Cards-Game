eed6192ca91b871f060726127d1e18d0a097ab71
Igor Bayerl <bayerl.igor@gmail.com>
2023-06-19 15:34:21 -0300

    change validation on join request

--- server/src/validation/roomEventsValidation.ts
+++ server/src/validation/roomEventsValidation.ts
@@ -4,7 +4,7 @@ import { z } from 'zod'
 export const JoinRequestSchema = z.object({
   username: z.string().min(1).max(20),
   pictureUrl: z.string(),
-  roomId: z.string().uuid(),
+  roomId: z.string().nonempty(),
   oldSocketId: z.string().optional(),
 })
 
