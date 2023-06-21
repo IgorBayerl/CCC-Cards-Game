264f2cd942828093f54402d8e078ce34e097e4f9
Igor Bayerl <bayerl.igor@gmail.com>
2023-06-21 09:01:03 -0300

    adding adsterra script

--- client/src/components/TrackingCode.tsx
+++ client/src/components/TrackingCode.tsx
@@ -5,14 +5,14 @@ export default function TrackingCode(): JSX.Element {
   return (
     <>
       <Script
-        strategy='lazyOnload'
+        strategy="lazyOnload"
         id="google-analytics"
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3354864508700429"
         crossOrigin="anonymous"
       />
       <Script
-        strategy='lazyOnload'
+        strategy="lazyOnload"
         id="google-tag-manager"
         async
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
@@ -26,6 +26,21 @@ export default function TrackingCode(): JSX.Element {
             gtag('js', new Date());
             gtag('config', ${GA_TRACKING_ID});
             `,
+        }}
+      />
+      <Script
+        id="adsterra"
+        dangerouslySetInnerHTML={{
+          __html: `
+            atOptions = {
+              'key' : '8fc0e4790d8173eaee59bfb8d2881922',
+              'format' : 'iframe',
+              'height' : 600,
+              'width' : 160,
+              'params' : {}
+            };
+            document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/8fc0e4790d8173eaee59bfb8d2881922/invoke.js"></scr' + 'ipt>');
+        `,
         }}
       />
     </>
