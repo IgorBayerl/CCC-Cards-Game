b93078ca28d0a045e18cf86bef9c4a3fa0cf52bc
IgorBayerl <dev.igorbayerl@gmail.com>
2024-03-10 14:59:25 +0000

    remember filters

--- client/src/components/Lobby/SearchDecks.tsx
+++ client/src/components/Lobby/SearchDecks.tsx
@@ -1,29 +1,48 @@
-import { type DeckFilters } from '@ccc-cards-game/types';
-import DeckFiltersSection from '../Atoms/DeckFilters';
-import { useEffect, useState } from 'react';
-import SearchDecksList from './SearchDecksList';
+import React, { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 import useTranslation from 'next-translate/useTranslation';
+import SearchDecksList from './SearchDecksList';
+import DeckFiltersSection from '../Atoms/DeckFilters';
+import { fetchLanguages } from '~/api/deck';
+import { type DeckFilters } from '@ccc-cards-game/types';
+import usePersistentState from '~/hooks/usePersistentState';
+
+const queryConfig = {
+  refetchOnWindowFocus: false,
+  refetchOnMount: false,
+};
 
 const SearchDecks: React.FC = () => {
   const { lang } = useTranslation();
-
-  const [deckFilters, setDeckFilters] = useState<DeckFilters>({
+  const { data: languages = [] } = useQuery<string[]>('get-languages', fetchLanguages, queryConfig);
+  const [deckFilters, setDeckFilters] = usePersistentState<DeckFilters>('deckFilters', {
     title: '',
     darknessLevel: [1, 2, 3],
-    language: ['pt'],
+    language: [],
   });
 
+  // State to track if the component has initialized.
+  const [initialized, setInitialized] = useState(false);
+
   useEffect(() => {
-    setDeckFilters((currentFilters) => ({
-      ...currentFilters,
-      language: [lang],
-    }));
-  }, [lang]);
+    // On component mount, check if deckFilters should update based on the current language
+    if (!initialized && languages.length > 0) {
+      const fallbackLanguage = languages.includes(lang) ? lang : languages[0] || 'en';
+
+      // Update only if the language list is empty
+      if (deckFilters.language.length === 0) {
+        setDeckFilters((currentFilters) => ({
+          ...currentFilters,
+          language: [fallbackLanguage],
+        }));
+      }
+      setInitialized(true);
+    }
+  }, [deckFilters.language.length, lang, languages, setDeckFilters, initialized]);
 
   return (
     <div className="flex flex-col gap-2 bg-opacity-50 text-accent max-h-full scrollbar-none">
-      <DeckFiltersSection
-        setFilters={setDeckFilters} filters={deckFilters} />
+      <DeckFiltersSection setFilters={setDeckFilters} filters={deckFilters} />
       <SearchDecksList filters={deckFilters} />
     </div>
   );
